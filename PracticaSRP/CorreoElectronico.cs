6cd3fc069f0aa70a5dfaa648978f720f1c9cf479
MichaelVR <michaelvasquesreyes03@gmail.com>
2024-12-02 21:03:18 -0400

    Segundo Commit

--- PracticaSRP/CorreoElectronico.cs
+++ PracticaSRP/CorreoElectronico.cs
@@ -10,7 +10,7 @@ namespace PracticaSRP
     {
         public void EnviarCorreoElectronico(string destinatario, string mensaje)
         {
-            // Lógica para enviar correo electrónico (por ejemplo, usando SMTP)
+            
             Console.WriteLine($"Enviando correo a {destinatario}: {mensaje}");
         }
     }
